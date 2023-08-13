829da3169ec8864058344479cc1f1a528de86d14
Augustinas <gungaudas@gmail.com>
2023-08-13 15:19:13 +0300

    Add arguments to workspace events

--- app/Events/Workspace/WorkspaceDeleted.php
+++ app/Events/Workspace/WorkspaceDeleted.php
@@ -2,6 +2,7 @@
 
 namespace App\Events\Workspace;
 
+use App\Models\Workspace;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -17,7 +18,7 @@ class WorkspaceDeleted
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(public Workspace $workspace)
     {
         //
     }
